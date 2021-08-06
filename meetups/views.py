f99fd299308ebab7e825c1bb359d346a4d7b9369
zunayed-arp <zunayed.arp@gmail.com>
2021-08-06 17:40:48 +0600

    template if else conditon,interpolation

--- meetups/views.py
+++ meetups/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'meetups/index.html')
+    meetups = [
+        {
+            'title': 'A First Meetup'
+        },
+        {
+            'title': "A Second Meetup"
+        }
+    ]
+    return render(request, 'meetups/index.html',{
+        'show_meetups': False,
+        'meetups':meetups
+    })
