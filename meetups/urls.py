c992e5abca24c13d8bcff4df45dc92039ddff872
zunayed-arp <zunayed.arp@gmail.com>
2021-08-08 15:18:41 +0600

    meetup page adde

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index)  # our-domain.com/meetups
+    path('meetups/', views.index),  # our-domain.com/meetups
+    path('meetups/<slug:meetup_slug>', views.meetup_details), # our-domain.com/meetup/<dynamic-path-segment>
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -20,3 +20,14 @@ def index(request):
         'show_meetups': True,
         'meetups': meetups
     })
+
+
+def meetup_details(request, meetup_slug):
+    selected_meetup = {
+        'title': 'A First Meetup',
+        'description': 'This is the first meetup.'
+    }
+    return render(request, 'meetups/meetup-details.html', {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['description']
+    })
