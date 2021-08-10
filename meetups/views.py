8c8e7f26b4979c59b964b07a743795c87cd91c59
zunayed-arp <zunayed.arp@gmail.com>
2021-08-10 15:04:49 +0600

    admin created meetup

--- meetups/views.py
+++ meetups/views.py
@@ -1,33 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Meetup
 
 
 def index(request):
-    meetups = [
-        {
-            'title': 'A First Meetup',
-            'location': 'New York',
-            'slug': 'a-first-meetup'
-
-        },
-        {
-            'title': "A Second Meetup",
-            'location': 'Paris',
-            'slug': 'a-second-meetup'
-        }
-    ]
+    meetups = Meetup.objects.all()
     return render(request, 'meetups/index.html', {
-        'show_meetups': True,
         'meetups': meetups
     })
 
 
 def meetup_details(request, meetup_slug):
-    selected_meetup = {
-        'title': 'A First Meetup',
-        'description': 'This is the first meetup.'
-    }
+    selected_meetup = Meetup.objects.get(slug=meetup_slug)
     return render(request, 'meetups/meetup-details.html', {
-        'meetup_title': selected_meetup['title'],
-        'meetup_description': selected_meetup['description']
+        'meetup_title': selected_meetup.title,
+        'meetup_description': selected_meetup.description
     })
