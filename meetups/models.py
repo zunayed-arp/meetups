d5dda531a7288bbabcd9aeb65277d4c39b9f65f1
zunayed-arp <zunayed.arp@gmail.com>
2021-08-11 00:09:09 +0600

    __str__ magic method modification"

--- meetups/models.py
+++ meetups/models.py
@@ -5,4 +5,6 @@ class Meetup(models.Model):
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
-    
+
+    def __str__(self):
+        return f'{self.title} - {self.slug}'
